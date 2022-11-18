c03aa0ef99f918a8f3e408db3dcaf8f7935c3f70
Oleg <o.niakhai@gmail.com>
2022-11-18 15:14:14 +0100

    c

---
+++ 1 lesson.ts
@@ -0,0 +1,22 @@
+const isFetching: boolean = true;
+const isLoading: boolean = false;
+
+const int: number = 42
+const float: number = 4.2
+const num: number = 3e10
+
+const message: string = 'Hello world!'
+
+const numberArray: number[] = [1, 2, 3, 4, 5, 6, 7]
+const numberArray2: Array<number> = [1, 2, 3, 4, 5, 6, 7]
+const words: string[] = ['Hello', 'Typescript']
+
+// Tuple
+const contact: [string, number] = ['Oleg' 123455]
+
+// Array
+let variable: any = 41
+
+//...
+variable = 'New String'
+variable = [1,2,3]
