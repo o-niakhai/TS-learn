3ff0fb3f149ddea136498d4d750ef1bf1c2c26a4
Oleg <o.niakhai@gmail.com>
2022-11-18 20:01:11 +0100

    2 lesson

--- 1 lesson.ts
+++ 1 lesson.ts
@@ -37,3 +37,14 @@ function infinite(): never {
 }
 
 //Type
+type Login = string
+
+const login: Login = 'admin'
+// const login2: Login = 2
+
+type ID = string | number
+const id1: ID = 1234
+const id2: ID = '1234'
+// const id3: ID = true
+
+type SomeType = string | null | undefined
